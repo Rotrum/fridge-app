8fb381f876c82cd9e688e4f8caeba9c8664d1699
Jake Chavis <jakechavis6@gmail.com>
2018-11-02 14:40:34 -0400

    first commit

---
+++ app/src/main/java/com/example/jchavis06/fridge_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jchavis06.fridge_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
